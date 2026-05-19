94050035ef4d99f78acc4699572bffee53f1a4ec
AshitaAme <senyouame@gmail.com>
2026-05-19 15:10:29 +0800

    chore: adjust display for home page

--- client/app/_components/cards-display.tsx
+++ client/app/_components/cards-display.tsx
@@ -2,10 +2,10 @@ import TagCard from './tag-card';
 
 const tagNameList = ['All', 'Computer Architecture', 'Frontend', 'Backend', 'AI', 'Data', 'Cloud Service', 'Git', 'Next.js']
 
-export default function cardsDisplay() {
+export default function CardsDisplay() {
     
     return (
-      <div className="grid pt-[6%] pb-[15%] md:grid-cols-2 md:pl-[20%] md:pr-[20%] lg:grid-cols-4 gap-10">
+      <div className="grid pt-[6.5%] lg:pt-[5.5%] pb-[15%] md:grid-cols-2 md:pl-[20%] md:pr-[20%] lg:grid-cols-4 gap-10">
         {tagNameList.map((tagName) => {
           return (
             <div key={tagName} className="flex items-center justify-center">
