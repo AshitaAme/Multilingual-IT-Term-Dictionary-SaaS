1aa99311a7702e22a6c1e11a81b14f8319fe2de2
AshitaAme <senyouame@gmail.com>
2026-08-05 14:40:09 +0800

    feat(saved): utilize high-order function to insert translator initialization before actions

--- client/features/saved/actions/add-review.action.ts
+++ client/features/saved/actions/add-review.action.ts
@@ -3,18 +3,37 @@
 import { addReview } from '@/shared/lib/db/mutations/review-card.mutations';
 import { StringArraySchema } from '../schemas/string-array.schema';
 import { toReviewCard } from '../types/review-card';
+import { getTranslations } from 'next-intl/server';
 
 export async function addReviewAction(savedTermIds: string[]) {
-  const parsed = StringArraySchema.safeParse(savedTermIds);
-  if (!parsed.success) return { success: false, error: 'Invalid input' };
+  // 1. Get i18n Translator
+  let t;
+  try {
+    t = await getTranslations('saved.errors');
+  } catch (err) {
+    console.warn('Fetch translator failed: ', err);
+  }
 
+  // 2. Zod validation
+  const parsed = StringArraySchema.safeParse(savedTermIds);
+  if (!parsed.success)
+    return { success: false, error: t ? t('invalidInput') : 'Invalid input' };
+
+  // 3. Add review
   try {
     const res = await addReview(parsed.data);
+
+    // 4. Map reviewCard to savedTermId
     const map = new Map();
     res.forEach((card) => map.set(card.savedTermId, toReviewCard(card)));
+
+    // 5. Success
     return { success: true, data: map };
   } catch (error) {
     console.error('[addReviewAction] Add review failed: ', error);
-    return { success: false, error: 'Add review failed' };
+    return {
+      success: false,
+      error: t ? t('addReviewFailed') : 'Add review failed',
+    };
   }
 }
--- client/features/saved/actions/delete-book.action.ts
+++ client/features/saved/actions/delete-book.action.ts
@@ -1,16 +1,29 @@
 'use server';
 
 import { deleteSavedBook } from '@/shared/lib/db/mutations/saved-book.mutations';
+import {
+  ServerTranslator,
+  withTranslations,
+} from '@/shared/lib/i18n/server-translations';
 
-export async function deleteBookAction(bookId: string) {
+async function deleteBookActionRaw(t: ServerTranslator, bookId: string) {
   if (!bookId || typeof bookId !== 'string')
-    return { success: false, error: 'Invalid input' };
+    return { success: false, error: t ? t('invalidInput') : 'Invalid input' };
 
   try {
     await deleteSavedBook(bookId);
     return { success: true };
   } catch (err) {
     console.error('[deleteBookAction] Delete book failed', err);
-    return { success: false, error: 'Delete book failed' };
+    return {
+      success: false,
+      error: t ? t('deleteFailed') : 'Delete book failed',
+    };
   }
 }
+
+// 导出包装后的 action
+export const deleteBookAction = withTranslations(
+  'saved.errors',
+  deleteBookActionRaw,
+);
