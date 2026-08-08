b5f5eb1f77117f4084e03c9bb8e0cfdea6145b61
AshitaAme <senyouame@gmail.com>
2026-08-08 15:32:29 +0800

    feat(saved): skeleton save term text logic

--- client/features/saved/components/term-modify-card.tsx
+++ client/features/saved/components/term-modify-card.tsx
@@ -6,13 +6,24 @@ import { Card, CardContent, CardTitle } from '@/shared/components/ui/card';
 import { FieldSeparator } from '@/shared/components/ui/field';
 import { useState } from 'react';
 import Editor from '@/shared/components/ui/editor';
+import { Button } from '@/shared/components/ui/button';
+import { saveTerms } from '@/features/search/services/save-term';
 
 export function TermModifyCard() {
   const term = useModifyStore((state) => state.modifiedTerm);
   const setModifiedTerm = useModifyStore((state) => state.setModifiedTerm);
   const [content, setContent] = useState(term?.text);
+  const [isSaving, setIsSaving] = useState(false);
 
   if (term === null) return;
+  const handleSaveText = async () => {
+    setIsSaving(true);
+    const res = await saveTermText({
+      savedTermId: term.savedTermId,
+      newText: content,
+    });
+    setIsSaving(false);
+  };
   return (
     <Card className="w-120 h-160 rounded-md relative bg-background p-4">
       <X
@@ -20,10 +31,17 @@ export function TermModifyCard() {
         onClick={() => setModifiedTerm(null)}
         className="absolute z-10 right-2.5 top-2.5 cursor-pointer"
       />
-      <CardTitle>{term.name}</CardTitle>
+      <CardTitle className="text-2xl px-2">{term.name}</CardTitle>
       <FieldSeparator />
-      <CardContent>
+      <CardContent className="flex flex-col gap-5.5">
         <Editor content={content} onChange={setContent} />
+        <Button
+          disabled={isSaving}
+          onClick={handleSaveText}
+          className="bg-muted-foreground/10 hover:bg-muted-foreground/20 text-foreground/60 hover:text-foreground"
+        >
+          Save
+        </Button>
       </CardContent>
     </Card>
   );
--- client/shared/components/ui/editor.tsx
+++ client/shared/components/ui/editor.tsx
@@ -4,6 +4,17 @@ import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import Placeholder from '@tiptap/extension-placeholder';
 import { Button } from './button';
+import {
+  AxeIcon,
+  Bold,
+  Italic,
+  LetterText,
+  LetterTextIcon,
+  List,
+} from 'lucide-react';
+import { TooltipWrapper } from './tooltipWrapper';
+import { cn } from '@/shared/utils/utils';
+import { FieldSeparator } from './field';
 
 export default function Editor({
   content = '',
@@ -28,36 +39,60 @@ export default function Editor({
   if (!editor) return null;
 
   return (
-    <div className="border rounded-lg p-4">
-      <div className="flex gap-2 mb-4 border-b pb-2">
-        <Button
-          onClick={() => editor.chain().focus().toggleBold().run()}
-          className={editor.isActive('bold') ? 'font-bold text-blue-600' : ''}
-        >
-          Bold
-        </Button>
-        <Button
-          onClick={() => editor.chain().focus().toggleItalic().run()}
-          className={editor.isActive('italic') ? 'italic text-blue-600' : ''}
-        >
-          Italic
-        </Button>
-        <Button
-          onClick={() =>
-            editor.chain().focus().toggleHeading({ level: 2 }).run()
-          }
-          className={
-            editor.isActive('heading', { level: 2 }) ? 'text-blue-600' : ''
-          }
-        >
-          Title
-        </Button>
-        <Button onClick={() => editor.chain().focus().toggleBulletList().run()}>
-          Bullet list
-        </Button>
-      </div>
+    <div className="border rounded-md">
+      <div className="flex items-center justify-start p-2">
+        <TooltipWrapper label={'Bold'} side="top">
+          <Button
+            variant="ghost"
+            onClick={() => editor.chain().focus().toggleBold().run()}
+            className={editor.isActive('bold') ? 'font-bold text-blue-600' : ''}
+          >
+            <Bold />
+          </Button>
+        </TooltipWrapper>
 
-      <EditorContent editor={editor} className="min-h-50 prose" />
+        <TooltipWrapper label={'Italic'} side="top">
+          <Button
+            variant="ghost"
+            onClick={() => editor.chain().focus().toggleItalic().run()}
+            className={editor.isActive('italic') ? 'italic text-blue-600' : ''}
+          >
+            <Italic />
+          </Button>
+        </TooltipWrapper>
+
+        <TooltipWrapper label={'Title'} side="top">
+          <Button
+            variant="ghost"
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 2 }).run()
+            }
+            className={
+              editor.isActive('heading', { level: 2 }) ? 'text-blue-600' : ''
+            }
+          >
+            <AxeIcon />
+          </Button>
+        </TooltipWrapper>
+
+        <TooltipWrapper label={'Bullet list'} side="top">
+          <Button
+            variant="ghost"
+            onClick={() => editor.chain().focus().toggleBulletList().run()}
+          >
+            <List />
+          </Button>
+        </TooltipWrapper>
+      </div>
+      <FieldSeparator />
+      <EditorContent
+        editor={editor}
+        className={cn(
+          'h-105 prose [&_.ProseMirror]:outline-none [&_.ProseMirror]:h-full [&_.ProseMirror]:min-h-full',
+          'rounded-md ring-muted-foreground/20',
+          'overflow-y-auto p-4',
+        )}
+      />
     </div>
   );
 }
